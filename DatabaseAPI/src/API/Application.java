c03cf18923a8f9b03608f559dd3d5385cf907615
breadonmars <qintutao@gmail.com>
2020-11-21 11:55:05 -0800

    added Selection Query

--- DatabaseAPI/src/API/Application.java
+++ DatabaseAPI/src/API/Application.java
@@ -1,7 +1,11 @@
 package API;
 
+import Database.RecipeTable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
--- DatabaseAPI/src/Database/RecipeTable.java
+++ DatabaseAPI/src/Database/RecipeTable.java
@@ -6,6 +6,7 @@ import oracle.ucp.common.waitfreepool.Tuple;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Formatter;
+import java.util.HashMap;
 import java.util.List;
 
 public class RecipeTable {
@@ -41,6 +42,7 @@ public class RecipeTable {
         return true;
     }
 
+    /* Aggregation with Group BY */
     public static String averageRating() {
         try (Connection connection = ConnectionFactory.createConnection()) {
             String query =
@@ -50,23 +52,78 @@ public class RecipeTable {
                     "GROUP BY c.typename";
             Statement statement = connection.createStatement();
             ResultSet result = statement.executeQuery(query);
-            // %-10s pads the argument to 10 characters. (the negative means left-aligned)
-            // Just to make the output look nicer and more like a table
-            // Also too lazy to make it an actual table in the frontend, I'd rather just print out a string
-            // that looks like a table lmao
-            StringBuilder table = new StringBuilder(String.format("\n%-10s%-10s", "FoodType", "Average Rating"));
-            while (result.next()) {
-                String foodtype = result.getString("typename");
-                String rating = result.getString("AVG(rating)");
-                String row = String.format("\n%-10s%-10s", foodtype, rating);
-                table.append(row);
-            }
-            System.out.println(table.toString());
-            return table.toString();
+
+            HashMap<String, String> table = new HashMap<>();
+            table.put("FoodType", "typename");
+            table.put("Average Rating", "AVG(rating");
+            return render(result, table);
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println(e.getMessage());
             return null;
         }
+    }
+
+    /**
+     * Selection Query, lists all recipes that satisfy:
+     * @param lo user defined lower bound, inclusive
+     * @param hi user defined upper bound, inclusive
+     * @return HTML string
+     */
+    public static String listRecipeWRating(int lo, int hi) {
+        try (Connection connection = ConnectionFactory.createConnection()) {
+            StringBuilder queryBuilder = new StringBuilder();
+            queryBuilder.append("SELECT r.rid")
+                    .append("FROM recipe r")
+                    .append("WHERE ").append(lo).append("<= r.rating AND r.rating <=").append(hi);
+            Statement statement = connection.createStatement();
+            ResultSet res = statement.executeQuery(queryBuilder.toString());
+            HashMap<String, String> table = new HashMap<>();
+            table.put("Rating ID", "rid");
+            return render(res, table);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * Private helper function that renders html elements
+     * @param result the query result set
+     * @param titles a list of table headers to be rendered, key is title, value is column label
+     * @return
+     */
+    public static String render(ResultSet result, HashMap<String, String> titles) throws Exception {
+        // %-10s pads the argument to 10 characters. (the negative means left-aligned)
+        // Just to make the output look nicer and more like a table
+        // Also too lazy to make it an actual table in the frontend, I'd rather just print out a string
+        // that looks like a table lmao
+
+        /* Origin: */
+//        StringBuilder table = new StringBuilder(String.format("\n%-10s%-10s", "FoodType", "Average Rating"));
+//        while (result.next()) {
+//            String foodtype = result.getString("typename");
+//            String rating = result.getString("AVG(rating)");
+//            String row = String.format("\n%-10s%-10s", foodtype, rating);
+//            table.append(row);
+//        }
+
+        /* format the table header */
+        StringBuilder table = new StringBuilder("\n");
+        for (String key: titles.keySet()) {
+            table.append(String.format("%-10s", titles.get(key)));
+        }
+
+        while (result.next()) {
+            StringBuilder row = new StringBuilder("\n");
+            for (String val: titles.values()) {
+                String elem = result.getString(val);
+                row.append(String.format("%-10s", elem));
+            }
+            table.append(row.toString());
+        }
+        System.out.println(table.toString());
+        return table.toString();
     }
 }
